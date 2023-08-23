796a428b5895f222a23ce43de2b30ad21fcb890c
mbz <30321314+mbz4@users.noreply.github.com>
2023-08-23 17:00:28 +0300

    python test assignment done

--- Python_test/DiffGNSSProcessor.py
+++ Python_test/DiffGNSSProcessor.py
@@ -31,8 +31,8 @@ class DiffGNSSProcessor: # class definition
             next(reader)
             for row in reader:
                 time_s = float(row[0])
-                x_m = float(int(row[1])) / 1000  # Convert to meters
-                y_m = float(int(row[2])) / 1000  # Convert to meters
+                x_m = float(int(row[1])) / 1000  # Convert to meters (makes more sense in this context)
+                y_m = float(int(row[2])) / 1000
                 roll_deg = float(row[3])
                 pitch_deg = float(row[4])
                 data_tuple = (time_s, x_m, y_m, roll_deg, pitch_deg)
@@ -65,7 +65,7 @@ class DiffGNSSProcessor: # class definition
             writer = csv.DictWriter(csvfile, 
                                     fieldnames = ['timestamp',
                                                   'x_m',   # Changed to meters
-                                                  'y_m',   # Changed to meters
+                                                  'y_m',   
                                                   'roll_deg', 
                                                   'pitch_deg', 
                                                   'heading_deg', 
@@ -75,13 +75,13 @@ class DiffGNSSProcessor: # class definition
                 writer.writerow({
                     'timestamp': self.data[i][0],
                     'x_m': self.data[i][1],   # Changed to meters
-                    'y_m': self.data[i][2],   # Changed to meters
+                    'y_m': self.data[i][2],   
                     'roll_deg': self.data[i][3],
                     'pitch_deg': self.data[i][4],
                     'heading_deg': self.headings[i],
                     'velocity': self.velocities[i]})
     
-    @time_it # measured taking approx 1ms (average) to complete 
+    @time_it # measured taking ~1ms to complete 
     def process_data(self): # process data
         self.data = sorted(self.data, key=lambda x: x[0])  # sort by timestamps
         self.calculate_projection()
@@ -97,27 +97,36 @@ class DiffGNSSProcessor: # class definition
         min_length = min(len(U), len(X)) # ugly hack to make sure all lists are the same length...
         U, V = U[:min_length], V[:min_length]
         X, Y = X[:min_length], Y[:min_length]
-        ax.quiver(X, Y, U, V, angles='xy', scale_units='xy', scale=8, color='b', alpha=0.3, width=0.002, headwidth=3, headlength=4, headaxislength=3.5)
-        ax.set_title("Quiver Plot with Projections")
+        ax.quiver(X, Y, U, V, angles='xy', scale_units='xy', scale=8, color='b', alpha=0.5, width=0.0045, headwidth=2.8, headlength=3.8, headaxislength=3.5)
+        ax.set_title("Speed & Heading on Moving Plane")
         ax.set_xlabel("X [m]")
         ax.set_ylabel("Y [m]")
 
     def plot_time_series(self, ax):
-        times = [point[0] for point in self.data]
-        roll = [point[3] for point in self.data]
-        pitch = [point[4] for point in self.data]
-        ax.plot(times, roll, label="Roll", color="blue")
-        ax.plot(times, pitch, label="Pitch", color="red")
-        ax.set_title("Roll & Pitch / Time")
+        times = [point[0] for point in self.data][1:]
+        roll = [point[3] for point in self.data][1:]
+        pitch = [point[4] for point in self.data][1:]
+        yaw = self.headings
+        # window_size = 5 # tried smoothing yaw but it didn't work well
+        # yaw = np.convolve(self.headings, np.ones(window_size)/window_size, mode='valid')
+        # adjusted_times = times[int(window_size/2): -int(window_size/2)]
+        ax.plot(times, roll, label="Roll", color="blue", alpha=0.5)
+        ax.plot(times, pitch, label="Pitch", color="red", alpha=0.5)
         ax.set_xlabel("Time [s]")
-        ax.set_ylabel("Angle [°]")
-        ax.legend() 
+        ax.set_ylabel("Roll & Pitch [°]")
+        ax.legend(loc='upper left')
+
+        ax2 = ax.twinx()
+        ax2.set_ylabel('Yaw [°]', color='green')
+        ax2.plot(times, yaw, label="Yaw", color="green", alpha=0.5)
+        ax2.tick_params(axis='y', labelcolor='green')
+        ax.set_title("Roll, Pitch, Yaw / Time")
 
     def plot_velocity_vs_time(self, ax):
         times = [point[0] for point in self.data] # get times
         smoothed_velocities = np.convolve(self.velocities, np.ones(5)/5, mode='valid') # simple moving average to smoothen the velocities
         truncated_times = times[len(times) - len(smoothed_velocities):] # truncate times to match the length of the smoothed velocities
-        ax.plot(truncated_times, smoothed_velocities, label="Velocity", color="purple")
+        ax.plot(truncated_times, smoothed_velocities, label="Velocity", color="purple", alpha=0.5)
         ax.set_title("Velocity / Time")
         ax.set_xlabel("Time [s]")
         ax.set_ylabel("Velocity [m/s]")
@@ -125,11 +134,17 @@ class DiffGNSSProcessor: # class definition
     def plot_polar(self, ax):
         angles = np.radians(self.headings)
         radii = self.velocities
-        ax.scatter(angles, radii, color='g', s=5)  # scatter plot with small points
-        ax.set_title("Heading [°] & Velocity [m/s]")
+        ax.scatter(angles, radii, color='g', s=20, alpha=0.5)
+        # for i, (angle, radius) in enumerate(zip(angles, radii)): # tried annotations for clarity but it was too cluttered
+        #     if i % 5 == 0:
+        #         ax.annotate(f"{radius:.2f} m/s", (angle, radius), textcoords="offset points", xytext=(0,5), ha='center', fontsize=8)   
+        ax.set_title("Heading [°] / Velocity [m/s]")
         ax.grid(True)
-        ax.set_theta_zero_location("S")  # Set 0 degrees to the top
-        ax.set_theta_direction(-1)  # Clockwise direction
+        ax.set_theta_zero_location("S")
+        ax.set_theta_direction(-1)
+        ax.set_rlim(0.4, None)
+        ax.set_thetamin(90)
+        ax.set_thetamax(180)
     
     def visualize_data(self, savefig):
 
@@ -144,10 +159,10 @@ class DiffGNSSProcessor: # class definition
         self.plot_time_series(ax3)
         self.plot_velocity_vs_time(ax4)
         
+        plt.subplots_adjust(hspace=0.3, wspace=0.3)
         plt.draw() # predraw the plot
         manager = plt.get_current_fig_manager() # get the current figure manager
         manager.window.showMaximized() # maximize the window
-        plt.tight_layout()
         if savefig:
             fig.savefig('analysis.png', dpi=300)
         plt.show() 
@@ -157,4 +172,4 @@ class DiffGNSSProcessor: # class definition
         self.visualize_data(savefig)
 
 if __name__ == "__main__":
-    DiffGNSSProcessor("input_data.csv").run(savefig=False)
+    DiffGNSSProcessor("input_data.csv").run(savefig=True)
